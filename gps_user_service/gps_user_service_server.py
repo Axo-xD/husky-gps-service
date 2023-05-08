8f5f3d6cfdacebe11e4ec777d107ee3668e05bf4
William <590413@stud.hvl.no>
2023-05-08 17:44:42 +0200

    Added working gps service for web app

---
+++ gps_user_service/gps_user_service_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import NavSatFix
+from gps_user_service.srv import SaveGPS
+
+
+def save_gps(req):
+    # Open a file for writing
+    f = open("gps_coordinates.txt", "w")
+    # Loop through the list of GPS coordinates in the request
+    for gps in req.gps_coordinates:
+        # Write the GPS coordinates to the file
+        f.write("{0},{1}\n".format(gps.latitude, gps.longitude))
+    # Close the file
+    f.close()
+    # Return a success message
+    return {"success": True}
+
+
+if __name__ == "__main__":
+    # Initialize the node
+    rospy.init_node("gps_user_service_server")
+    # Create the service
+    rospy.Service("save_gps", SaveGPS, save_gps)
+    # Spin the node
+    rospy.spin()
