9ed76a4fd4d444be4ddb8b091fc124c3fb8e9c33
William <590413@stud.hvl.no>
2023-05-08 18:26:28 +0200

    Saves file to packet in src folder

--- gps_user_service/gps_user_service_server.py
+++ gps_user_service/gps_user_service_server.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import rospy
+import os
 from sensor_msgs.msg import NavSatFix
 from gps_user_service.srv import SaveGPS
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def save_gps(req):
     # Open a file for writing
-    f = open("gps_coordinates.txt", "w")
+    #f = open("gps_coordinates.txt", "w")
+    f = open(os.path.join(dir_path, 'gps_coordinates.txt'), 'w')
     # Loop through the list of GPS coordinates in the request
     for gps in req.gps_coordinates:
         # Write the GPS coordinates to the file
